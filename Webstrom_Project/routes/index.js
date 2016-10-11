17d9eae702fc7a2a994f7022ae4e9ddbaef2c00f
SongWooseok <wooseok01@naver.com>
2016-10-11 17:51:04 +0900

    prototype for Raja's design work

---
+++ Webstrom_Project/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
